07b0ec9c17ab48a2dca179c24fa7db17f1d224fa
Richard Castro Júnior <richard.castro@unesp.br>
2025-06-16 15:56:36 -0300

    fiz as correções sugeridas

--- aula03_exercicios.py
+++ aula03_exercicios.py
@@ -9,17 +9,23 @@ for multi in range(1, 11):
 
 ###########Problema 2: Jogo de Adivinhação######################
 
+# Versão Aprimorada (Padrão com 'while True' e 'break')
 import random
 numero_secreto = random.randint(1, 100)
 
-palpite=False
+print("--- Jogo de Adivinhação ---")
+print("Eu pensei em um número entre 1 e 100. Tente adivinhar!")
 
-while palpite != numero_secreto:
+while True: # Cria um laço que, a princípio, rodaria para sempre
     palpite = int(input("Seu palpite: "))
-    
+
     if palpite == numero_secreto:
-        print(f"Parabéns, você acertou!")
+        print("Parabéns, você acertou!")
+        break # A condição de saída foi atingida, então QUEBRAMOS o laço.
     elif palpite < numero_secreto:
-        print(f"Muito baixo!")
+        print("Muito baixo! Tente novamente.")
     else:
-        print(f"Muito alto!")
+        print("Muito alto! Tente novamente.")
+
+# O código continua aqui depois que o 'break' é executado
+print("Fim de jogo!")
